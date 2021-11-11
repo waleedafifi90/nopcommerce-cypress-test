d9e619c9d40d9c85686787977994169fc0f62cf9
abdullahSaadeddin <abdullahsaadeddin.yt@gmail.com>
2021-11-11 01:43:57 -0800

    add the first test

---
+++ cypress/integration/firstExample.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("learn how to use the selectros",()=>{
+
+    it("best practices of selecting elemnts",()=>{
+        const url = "https://s"
+        cy.visit(url)
+    })
+
+
+    
+})
